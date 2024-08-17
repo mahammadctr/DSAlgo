f08d444bc36f931125e0e70a659c556527204a08
mahammadhussain98 <141554718+mahammadhussain98@users.noreply.github.com>
2024-08-17 18:13:21 +0530

    Time: 0 ms (100%), Space: 7.4 MB (15%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -1,6 +1,31 @@
+
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        return false;
+        if (n < 4) return false;
+
+        auto isPalindromic = [](string s) {
+            int left = 0, right = s.size() - 1;
+            while (left < right) {
+                if (s[left] != s[right]) return false;
+                ++left;
+                --right;
+            }
+            return true;
+        };
+
+        for (int base = 2; base <= n - 2; ++base) {
+            string representation = "";
+            int temp = n;
+            while (temp > 0) {
+                int digit = temp % base;
+                representation += (digit < 10 ? '0' + digit : 'a' + digit - 10);
+                temp /= base;
+            }
+            reverse(representation.begin(), representation.end());
+            if (!isPalindromic(representation)) return false;
+        }
+
+        return true;
     }
 };
