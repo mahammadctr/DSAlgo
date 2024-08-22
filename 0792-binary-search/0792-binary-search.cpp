2e73a9aa2c63219445c9a900d27286cefbc8fc97
mahammadhussain98 <141554718+mahammadhussain98@users.noreply.github.com>
2024-08-22 22:34:28 +0530

    Time: 19 ms (94.5%), Space: 30.1 MB (52.17%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(target==nums[i])
+            index=i;
+        }
+        return index;
+    }
+};
