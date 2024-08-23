6df1c9060aa3043be27b1bf60a62a22918ed1853
mahammadhussain98 <141554718+mahammadhussain98@users.noreply.github.com>
2024-08-23 11:20:05 +0530

    Time: 3 ms (71.09%), Space: 12.1 MB (70.79%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -7,11 +7,11 @@ public:
           int  mid=(low+high)/2;
            if(target==nums[mid]){
            return mid;}
-           else if(nums[mid] < target){
-             low=mid+1;
+           else if(nums[mid] > target){
+             high=mid -1;
              }
         else {
-            high=mid-1;}
+            low=mid+1;}
             }
         
         return low;
