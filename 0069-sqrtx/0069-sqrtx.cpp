4f51efb4dfee37d6ad431463716d86b372935d60
mahammadhussain98 <141554718+mahammadhussain98@users.noreply.github.com>
2024-08-27 19:13:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      int ans=0;
-      for(long long i=1;i<=x;i++){
-    long long val=i*i;
-    if(val<=x*1ll){
-        ans=i;
-    }
-    else {
-        break;
-    }
-      } 
-      return ans; 
+        int ans=sqrt(x);
+        return ans;
     }
 };
